ce745830013a2c57484b807f21ceb6becb4c3a3a
3DM <dimasdaniswara02@gmail.com>
2024-06-07 18:41:34 +0700

    Develop toggle todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
-import Todos from './components/Todos';  // Lalukan Import
+import Todos from './components/Todos';
 
 function App() {
   const [todos, setTodos] = useState([
     {
       id: 1,
       title: 'Finish Progate React Course',
-      completed: false,
+      completed: true,
     },
     {
       id: 2,
@@ -18,17 +18,36 @@ function App() {
       title: 'Study React with Ninja Ken',
       completed: false,
     },
-  ])
+  ]);
 
-  console.log(todos)
+
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        console.log(todo);
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(updatedTodos);
+  }
 
   return (
-    // Menambah CSS
-    <div style={{ textAlign: 'center', padding: '12px' }}>
-      <h1 style={{fontSize: '36px'}}>My Todo List</h1>
-      <Todos todos={todos} />
+    <div style={styles.container}>
+      <h1 style={styles.title}>My Todo List</h1>
+      <Todos todos={todos} toggleCompleted={toggleCompleted} />
     </div>
   )
+}
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    padding: '12px',
+  },
+  title: {
+    fontSize: '36px',
+  },
 }
 
 export default App
